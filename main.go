80a948cb0638c31fc7306313eabfaff448cfff8a
Andrew Holmes <andrewholmes011002@gmail.com>
2024-04-09 10:07:33 -0400

    html and server functional

--- main.go
+++ main.go
@@ -2,22 +2,35 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 )
 
 const portNumber = ":8080"
 
+type Word struct {
+	Term         string
+	PartOfSpeech string
+	Definition   string
+}
+
 func handlerHome(w http.ResponseWriter, r *http.Request) {
-	log.Printf("Received the following request: %v", r.Body)
-	_, err := fmt.Fprintf(w, "Home page")
-	if err != nil {
-		log.Panic(err)
+	data := Word{
+		Term:         "dog",
+		PartOfSpeech: "noun",
+		Definition:   "Four legged animal descending from wolves.",
 	}
+
+	tmpl, err := template.ParseFiles("templates/index.html")
+	if err != nil {
+		log.Fatal(err)
+	}
+	tmpl.Execute(w, data)
 }
 
 func main() {
-	log.Printf("HTTP server starting on localhost%v", portNumber)
+	fmt.Printf("HTTP server listening on localhost%v\n", portNumber)
 	http.HandleFunc("/", handlerHome)
 	err := http.ListenAndServe(portNumber, nil)
 	if err != nil {
